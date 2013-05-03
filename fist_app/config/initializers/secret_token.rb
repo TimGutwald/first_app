638381591d06ccc8e0f1cca37d59fa75d0d1a0fa
Tim Gutwald <tim.gutwald@gmail.com>
2013-05-03 14:46:15 -0600

    Initial Commit

---
+++ fist_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FistApp::Application.config.secret_token = '2c489bfd98e570bb840f6f13073e66c09e3fff964fea77057d45d816864ebbc9dd2a447371225cfca007942c21c3b5bd39cd9a2acd4ff736afcd463ce25d9fd0'
---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f9967875034b61e8ca3467701fac86485e0d17e04b92dacaa2be361c36d3afc6cacae02ff63bc3806a4e359bce2f1865355714addd13e8f372ad07fd2133020f'
